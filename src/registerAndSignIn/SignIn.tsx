12ec52be3dc255648866e44156eadc2ffc5c7906
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-25 13:17:51 -0500

    settings up sign-in buttons

--- src/registerAndSignIn/SignIn.tsx
+++ src/registerAndSignIn/SignIn.tsx
@@ -1,7 +1,14 @@
+import { Form } from "react-bootstrap"
 const SignIn = () => {
 	return (
 		<div>
 			<h1>Sign In</h1>
+			<Form >
+        <Form.Label htmlFor="email">Email</Form.Label>
+				<Form.Control type="email" placeholder="Email" required={true} />
+        <Form.Label htmlFor="password">Password</Form.Label>
+				<Form.Control type="password" placeholder="Password" required={true} />
+			</Form>
 		</div>
 	)
 }
