b8c63283bbd25927418815d2f6a4c359d79cec4d
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-26 23:19:27 -0500

    set up function to check refresh token on clicking sign in.  backend doesn't seem to be saving tokens to cookies properly

--- src/registerAndSignIn/SignUpInButtons.tsx
+++ src/registerAndSignIn/SignUpInButtons.tsx
@@ -1,18 +1,50 @@
-import React from "react"
+import axios from "axios"
+import { useAppContext } from "../AppContext"
+import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import { Form, Button } from "react-bootstrap"
 
 const SignUpInButtons: React.FC = () => {
+	const { appState, setAppState } = useAppContext()
+	const navigate = useNavigate()
+	const checkForRefreshToken = async () => {
+		try {
+			const response = await axios.post(`${appState.backendUrl}/auth/refresh`)
+			debugger
+			if (response.status === 200) {
+				debugger
+				setAppState({
+					...appState,
+					userIsLoaded: true,
+					userInfo: {
+						name: response.data.name,
+						username: response.data.username,
+						avatar: "https://i.pravatar.cc/300",
+						id: response.data.id,
+					},
+				})
+				debugger
+				navigate("/users")
+				return true
+			}
+			debugger
+			return false
+		} catch (error) {
+			debugger
+			console.log(error)
+		}
+	}
+
 	return (
 		<div className="app-body">
 			<Form className="sign-in-up-container">
 				<Link to="/signup" className="react-router-Link">
-					<Button className="button">
-						Sign Up
-					</Button>
+					<Button className="button">Sign Up</Button>
 				</Link>
 				<Link to="/signin" className="react-router-Link">
-					<Button className="button">Sign In</Button>
+					<Button className="button" onClick={checkForRefreshToken}>
+						Sign In
+					</Button>
 				</Link>
 			</Form>
 		</div>
