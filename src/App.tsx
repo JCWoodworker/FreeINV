ad3ec9310645835562fb48d077f50b102744f7b4
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-27 16:51:34 -0500

    custom hooks set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "./main.scss"
+import { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
+import { useAppContext } from "./AppContext"
+import { useLocalStorageState } from "./customHooks/useLocalStorageState"
+
 import TopNav from "./topNav/TopNav"
 import UsersIndex from "./users/UsersIndex"
 import HomePage from "./homePage/HomePage"
@@ -7,6 +12,27 @@ import SignIn from "./registerAndSignIn/SignIn"
 import SignUp from "./registerAndSignIn/SignUp"
 
 function App() {
+	const { appState, setAppState } = useAppContext()
+	const [localStorageState, setLocalStorageState] = useLocalStorageState(
+		"invUserInfo",
+		appState.userInfo
+	)
+	console.log(localStorageState)
+
+	useEffect(() => {
+		if (localStorageState) {
+			setAppState(localStorageState.appState)
+		}
+	}, [])
+
+	useEffect(() => {
+		if (appState.userIsLoaded) {
+			setLocalStorageState({
+				appState,
+			})
+		}
+	}, [appState])
+
 	return (
 		<>
 			<TopNav />
