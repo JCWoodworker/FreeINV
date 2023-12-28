cb86ed89ea6e5770a002d3cad347a9c45fd2eba9
JCWoodworker <jc@rilocalwoodworks.com>
2023-12-28 14:32:36 -0500

    added sign in form

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -2,9 +2,23 @@ import GoogleOAuth from "./GoogleOAuth"
 
 const SignIn = () => {
 	return (
-		<>
+		<div className="GoogleOAuth">
+			<h1>Sign In</h1>
+			<form
+				className="sign-in-form"
+				onSubmit={() => {
+					alert("Clicked Submit ... nothing happened!   This doesn't work yet!")
+				}}
+			>
+				<input type="text" placeholder="Email" />
+				<input type="password" placeholder="Password" />
+				<button>Sign In</button>
+			</form>
+			<br />
+			<h3>OR</h3>
+
 			<GoogleOAuth />
-		</>
+		</div>
 	)
 }
 
