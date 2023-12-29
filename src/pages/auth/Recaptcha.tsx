5965a75bb1f7bf71e5d4c66becdeb81622858c8a
JCWoodworker <jc@rilocalwoodworks.com>
2023-12-29 01:05:01 -0500

    resized and themed captcha

--- src/pages/auth/Recaptcha.tsx
+++ src/pages/auth/Recaptcha.tsx
@@ -1,16 +1,18 @@
 import ReCAPTCHA from "react-google-recaptcha"
 
 interface Props {
-  setRecaptchaVerified: (value: boolean) => void
+	setRecaptchaVerified: (value: boolean) => void
 }
 
-const Recaptcha: React.FC<Props> = ({ setRecaptchaVerified}) => {
+const Recaptcha: React.FC<Props> = ({ setRecaptchaVerified }) => {
 	const onRecaptchaChange = (value: unknown) => {
 		setRecaptchaVerified(value !== "")
 	}
 
 	return (
 		<ReCAPTCHA
+			size="compact"
+			theme="dark"
 			sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
 			onChange={onRecaptchaChange}
 		/>
