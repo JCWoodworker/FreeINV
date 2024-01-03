19f6ed29640de068233dec157c0a981c778ac560
JCWoodworker <jc@rilocalwoodworks.com>
2024-01-03 16:12:31 -0500

    removed useState

--- src/pages/inventory/InventoryShow.tsx
+++ src/pages/inventory/InventoryShow.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { useLocation, Link } from "react-router-dom"
 import { InventoryRecord } from "../../routes/testUserData"
 import { userData } from "../../routes/testUserData"
