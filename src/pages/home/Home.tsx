c000ed11fe578bf9c6b7bbffbd8a0747e3d0f6ef
JCWoodworker <jc@rilocalwoodworks.com>
2024-01-03 06:35:34 -0500

    added subscription levels on home page with links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 interface Props {
 	loggedInUser: unknown
 }
@@ -16,7 +18,10 @@ const Home: React.FC<Props> = ({ loggedInUser }) => {
 			<h1>Welcome to Free INV!</h1>
 			<p>Your FREE inventory management application</p>
 			<ul>
-				All levels include:
+				All subscription levels include:
+				<li>
+					<strong className="strong-no">$0</strong> Monthly Fees
+				</li>
 				<li>
 					<strong className="strong-no">NO</strong> In-App Ads
 				</li>
@@ -28,47 +33,49 @@ const Home: React.FC<Props> = ({ loggedInUser }) => {
 				</li>
 			</ul>
 			<div className="home-description-container">
-				<div className="item-description-container">
-					<h3>Level 1</h3>
-					<ul>
-						<li>1 Location</li>
-						<li>Up to 2 Rooms</li>
-						<li>Up to 5 Items Per Room</li>
-						<br />
-						<li>Twice a month blog emails with affiliate links</li>
-						<li>You may also fully opt-out of our emails at this level!</li>
-					</ul>
-				</div>
-				<div className="item-description-container">
-				<h3>Level 2</h3>
-					<ul>
-						<li>2 Locations</li>
-						<li>Up to 2 Rooms Per Location</li>
-						<li>Up to 10 Items Per Room</li>
-						<br />
-						<li>Weekly blog emails with affiliate links</li>
-					</ul>
-				</div>
-				<div className="item-description-container">
-				<h3>Level 3</h3>
-					<ul>
-						<li>Up to 4 Locations</li>
-						<li>Up to 4 Rooms Per Location</li>
-						<li>Up to 50 Items Per Room</li>
-						<br />
-						<li>Twice a week blog emails with affiliate links</li>
-					</ul>
-				</div>
-				<div className="item-description-container">
-				<h3>Level 4</h3>
-					<ul>
-						<li>Up to 8 Location</li>
-						<li>Up to 5 Rooms Per Location</li>
-						<li>Up to 10 Items per room</li>
-						<br />
-						<li>Daily blog emails with affiliate links</li>
-					</ul>
-				</div>
+				<Link to="/signin" state={"level 1"}>
+					<div className="item-description-container">
+						<h3>Level 1</h3>
+						<ul>
+							<li>1 Location</li>
+							<li>Up to 2 Rooms</li>
+							<li>Up to 5 Items Per Room</li>
+							<br />
+							<li>
+								You'll receive blog emails with affiliate links twice per month
+							</li>
+							<li>You may also fully opt-out of our emails at this level!</li>
+						</ul>
+					</div>
+				</Link>
+				<Link to="/signin" state={"level 2"}>
+					<div className="item-description-container">
+						<h3>Level 2</h3>
+						<ul>
+							<li>2 Locations</li>
+							<li>Up to 4 Rooms Per Location</li>
+							<li>Up to 20 Items Per Room</li>
+							<br />
+							<li>
+								Must opt in to receive weekly blog emails with affiliate links
+							</li>
+						</ul>
+					</div>
+				</Link>
+				<Link to="/signin" state={"level 3"}>
+					<div className="item-description-container">
+						<h3>Level 3</h3>
+						<ul>
+							<li>Up to 8 Locations</li>
+							<li>Up to 5 Rooms Per Location</li>
+							<li>Up to 10 Items per room</li>
+							<br />
+							<li>
+								Must opt in to receive daily blog emails with affiliate links
+							</li>
+						</ul>
+					</div>
+				</Link>
 			</div>
 		</div>
 	)
