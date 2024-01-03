a0c98d8fc2b053d422cd7aa5b03fbc6d1aad0b9d
JCWoodworker <jc@rilocalwoodworks.com>
2024-01-03 06:55:30 -0500

    modified styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,45 +1,40 @@
 import { Link } from "react-router-dom"
+import { LoggedInUser } from "../../App"
 
 interface Props {
-	loggedInUser: unknown
+	loggedInUser: LoggedInUser
 }
 
 const Home: React.FC<Props> = ({ loggedInUser }) => {
-	if (!loggedInUser) {
+	if (loggedInUser.id === undefined) {
 		return (
 			<div>
-				<h1>Please login</h1>
-			</div>
-		)
-	}
-
-	return (
-		<div>
+						<div>
 			<h1>Welcome to Free INV!</h1>
 			<p>Your FREE inventory management application</p>
 			<ul>
 				All subscription levels include:
 				<li>
-					<strong className="strong-no">$0</strong> Monthly Fees
+					<strong className="strong-highlight">$0</strong> Monthly Fees
 				</li>
 				<li>
-					<strong className="strong-no">NO</strong> In-App Ads
+					<strong className="strong-highlight">NO</strong> In-App Ads
 				</li>
 				<li>
-					<strong className="strong-no">NO</strong> Unnecessary Notifications
+					<strong className="strong-highlight">NO</strong> Unnecessary Notifications
 				</li>
 				<li>
-					<strong className="strong-no">NO</strong> In-App Purchases
+					<strong className="strong-highlight">NO</strong> In-App Purchases
 				</li>
 			</ul>
 			<div className="home-description-container">
 				<Link to="/signin" state={"level 1"}>
 					<div className="item-description-container">
-						<h3>Level 1</h3>
+						<h2>Level 1</h2>
 						<ul>
-							<li>1 Location</li>
-							<li>Up to 2 Rooms</li>
-							<li>Up to 5 Items Per Room</li>
+							<li><strong className="strong-highlight">1</strong> Location</li>
+							<li>Up to <strong className="strong-highlight">2</strong> Rooms</li>
+							<li>Up to <strong className="strong-highlight">10</strong> items per room</li>
 							<br />
 							<li>
 								You'll receive blog emails with affiliate links twice per month
@@ -50,11 +45,11 @@ const Home: React.FC<Props> = ({ loggedInUser }) => {
 				</Link>
 				<Link to="/signin" state={"level 2"}>
 					<div className="item-description-container">
-						<h3>Level 2</h3>
+						<h2>Level 2</h2>
 						<ul>
-							<li>2 Locations</li>
-							<li>Up to 4 Rooms Per Location</li>
-							<li>Up to 20 Items Per Room</li>
+							<li>Up to <strong className="strong-highlight">2</strong> locations</li>
+							<li>Up to <strong className="strong-highlight">4</strong> rooms per location</li>
+							<li>Up to <strong className="strong-highlight">40</strong> items per room</li>
 							<br />
 							<li>
 								Must opt in to receive weekly blog emails with affiliate links
@@ -64,11 +59,11 @@ const Home: React.FC<Props> = ({ loggedInUser }) => {
 				</Link>
 				<Link to="/signin" state={"level 3"}>
 					<div className="item-description-container">
-						<h3>Level 3</h3>
+						<h2>Level 3</h2>
 						<ul>
-							<li>Up to 8 Locations</li>
-							<li>Up to 5 Rooms Per Location</li>
-							<li>Up to 10 Items per room</li>
+							<li>Up to <strong className="strong-highlight">8</strong> locations</li>
+							<li>Up to <strong className="strong-highlight">5</strong> rooms per location</li>
+							<li>Up to <strong className="strong-highlight">50</strong> items per room</li>
 							<br />
 							<li>
 								Must opt in to receive daily blog emails with affiliate links
@@ -78,6 +73,13 @@ const Home: React.FC<Props> = ({ loggedInUser }) => {
 				</Link>
 			</div>
 		</div>
+
+			</div>
+		)
+	}
+
+	return (
+		<h1>Welcome Back!</h1>
 	)
 }
 
