42abf26840d7862f6dcd911dc1f1c990fdbfbc56
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-01 00:00:11 -0500

    added back room list in location show

--- src/pages/inventory/locations/LocationShow.tsx
+++ src/pages/inventory/locations/LocationShow.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { ListGroup } from "react-bootstrap"
 
 import NotFound from "../../not-found/NotFound"
 import BackButton from "../../../components/BackButton"
@@ -24,6 +26,32 @@ const LocationShow: React.FC = () => {
 				<h1>{currentLocation?.name}</h1>
 				<p>{currentLocation?.description}</p>
 			</div>
+			<ListGroup className="m-2 w-100">
+				{currentLocation?.rooms?.map((room) => (
+					<div
+						key={room.id}
+						className="w-100 d-flex flex-row justify-content-center align-items-center text-center"
+					>
+						<Link
+							to={`/my-inventory/rooms/${room.id}`}
+							state={{
+								locationId: id,
+								roomId: room.id,
+								locationName: currentLocation?.name,
+							}}
+							className="w-sm-100, w-50"
+						>
+							<ListGroup.Item
+								action variant="light"
+								key={room.id}
+								className="m-1 rounded"
+							>
+								<strong>{room.name}</strong>
+							</ListGroup.Item>
+						</Link>
+					</div>
+				))}
+			</ListGroup>
 			<div className="m-2 d-flex flex-column justify-content-center align-items-center">
 				<AddDeleteButton
 					buttonAction="add"
