ab0df6170b6b322e2be560256bb3a5314f14fa4c
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-14 18:53:20 -0500

    updated all user data endpoint text

--- src/utils/hydrateUserData.ts
+++ src/utils/hydrateUserData.ts
@@ -7,7 +7,7 @@ export const hydrateUserData = async (
 	setUserInventoryData: React.SetStateAction<any>
 ) => {
 	const response: UserLocationData[] = await Request.get(
-		"/subapps/freeinv/complete-location",
+		"/subapps/freeinv/all-user-data",
 		true,
 		accessToken
 	)
