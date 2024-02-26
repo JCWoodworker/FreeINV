bb0885a6d02e49664bab5d169047e1854be34202
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-26 08:29:02 -0500

    removed media card

--- src/pages/inventory/02-rooms/RoomShow.tsx
+++ src/pages/inventory/02-rooms/RoomShow.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { Room, Item } from "../inventoryTypes"
 import { Link, useParams } from "react-router-dom"
-import { CardMedia, Typography } from "@mui/material"
+import { Typography } from "@mui/material"
 
 import NotFound from "../../not-found/NotFound"
 import AddDeleteButton from "../../../components/AddDeleteButton"
