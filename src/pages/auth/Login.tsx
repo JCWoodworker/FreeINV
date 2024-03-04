c2f8e8627e1394d3480c2c08cd5beb94053c210f
JCWoodworker <jc@rilocalwoodworks.com>
2024-03-04 08:27:06 -0500

    updated login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,11 +1,23 @@
+import { Box, Typography } from "@mui/material"
 import GoogleOAuth from "./GoogleOAuth"
 const Login = () => {
-
 	return (
-		<div>
-			<h1>Sign In with Google</h1>
-			<GoogleOAuth signUpOrIn="signin"/>
-		</div>
+		<Box
+			sx={{
+				textAlign: "center",
+				mt: 1,
+				p: 2,
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Typography variant="h3" sx={{ mb: 3 }}>
+				Sign In with Google
+			</Typography>
+			<GoogleOAuth signUpOrIn="signin" />
+		</Box>
 	)
 }
 
